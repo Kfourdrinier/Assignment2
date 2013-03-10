249fdab62f3ad55c6f03483b18eb787085ccc8b5
FOURDRINIER Kevin <fourdrinier.kevin@gmail.com>
2013-03-10 20:06:49 +0000

    Add a video

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+ render_views
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -9,18 +10,102 @@ describe PagesController do
     end
   end
 
-  describe "GET 'wiki1'" do
+  describe "GET 'biography'" do
     it "returns http success" do
-      get 'wiki1'
+      get 'biography'
       response.should be_success
     end
   end
 
-  describe "GET 'wiki2'" do
+  describe "GET 'filmography'" do
     it "returns http success" do
-      get 'wiki2'
+      get 'filmography'
       response.should be_success
     end
   end
+  
+  describe "Make a test for the Home Page" do
+
+	it "has good title" do
+		get 'home'
+		response.should have_selector("title", :content => "Jim Carrey - Wiki")
+	end
+
+	it "has header" do
+		get 'home'
+		response.should have_selector("header")
+	end
+
+	it "has footer" do
+		get 'home'
+		response.should have_selector("footer")
+	end
+
+	it "has nav" do
+		get 'home'
+		response.should have_selector("nav")
+	end
+end
+
+describe "Make a test for the First Page" do
+
+	it "has good title" do
+		get 'biography'
+		response.should have_selector("title", :content => "Jim Carrey - Wiki")
+	end
+
+	it "has header" do
+		get 'biography'
+		response.should have_selector("header")
+	end
+
+	it "has footer" do
+		get 'biography'
+		response.should have_selector("footer")
+	end
+
+	it "has nav" do
+		get 'biography'
+		response.should have_selector("nav")
+	end
+end
+
+describe "Make a test for the Second Page" do
+
+	it "has good title" do
+		get 'filmography'
+		response.should have_selector("title", :content => "Jim Carrey - Wiki")
+	end
+
+	it "has header" do
+		get 'filmography'
+		response.should have_selector("header")
+	end
+
+	it "has footer" do
+		get 'filmography'
+		response.should have_selector("footer")
+	end
+
+	it "has nav" do
+		get 'filmography'
+		response.should have_selector("nav")
+	end
+end
+
+describe "Test all routes for wiki" do
+
+	it "routes to home" do
+		expect(:get => "/").to route_to(:controller => "pages", :action => "home")
+	end
+
+	it "routes to the first page" do
+		expect(:get => "/pages/biography").to route_to(:controller => "pages", :action => "biography")
+	end
+
+	it "routes to the second page" do
+		expect(:get => "/pages/filmography").to route_to(:controller => "pages", :action => "filmography")
+	end	
+end
 
 end
